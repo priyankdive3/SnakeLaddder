9fc70d1ff4403d93854414bfdb89107a1ec67c3d
priyanka <priyanka.divekar1100@gmail.com>
2022-08-30 19:53:25 +0530

    [Priyanka]Roll a die

---
+++ SnakeLadder/DieRoll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class DieRoll
+    {
+        Random random = new Random();
+        public int RollDie()
+        {
+            int dieCount = random.Next(1, 7);
+            return dieCount;
+        }
+    }
+}
